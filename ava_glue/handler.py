ed2e7fc1da8536a3aa884f11bcde256bc45dc416
aidangannon <aidanwilliamgannon@gmail.com>
2026-05-27 19:29:58 +0000

    Add bruno stuff as well as lambda fix

--- ava_glue/handler.py
+++ ava_glue/handler.py
@@ -1,6 +1,11 @@
 # pyright: basic
 # ruff: noqa
 
+import json
+
 def handle(event, _):
     print(event)
-    return event
+    return {
+        "statusCode": 200,
+        "body": json.dumps(event)
+    }
